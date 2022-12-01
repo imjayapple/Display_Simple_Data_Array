617f2d6a2c91623c1c03930dffa768f6a8d643f1
imjayapple <thisisfakeemail@gmai.com>
2022-11-30 21:42:33 -0800

    Use JavaScript to make the datasets appear in the HTML as divs using data.map

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ let data = [
     {
         name: 'Yanis',
         age: '29'
-    },
+    }
 ];
 
-const info = document.querySelector('#info')
+const info = document.querySelector('#info');
 
 let details = data.map(function(item) {
-    return '<div>' + item.name + ' ' + 'is ' + item.age '</div>';
+    return '<div>' + item.name + ' ' + 'is ' + item.age + ' years old' + '</div>';
 });
 
 info.innerHTML = details.join('\n');
